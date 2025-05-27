75e4f8b78c9ce33525e2f841dfd077cc077b87cb
Anuda Ransara <anudaransara1@gmail.com>
2025-05-27 23:11:33 +0530

    create user model.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-
+import mongoose from 'mongoose'
+import { error } from "console";
 
 const app = express();
 
@@ -20,6 +21,22 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
     console.log("Server is running on http://localhost:8080/");
+});
+
+const MONGO_URL = 'mongodb+srv://anudaransara1:NzAobuY9VGnmD8O0@cluster0.qej00x4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+});
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected successfully');
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected');
 });
 
 
